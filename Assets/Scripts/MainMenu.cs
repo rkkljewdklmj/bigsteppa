956b0f1b3d598c9b2f5fa4f79cf1b82ab25ea1a2
harry <146829307+rkkljewdklmj@users.noreply.github.com>
2024-05-16 16:30:56 +0100

    golem update

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public GameObject Fade;
+    public float duration = 3;
+
    public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        Fade.SetActive(true);
+        StartCoroutine(Fadeout());
+
     }
 
     public void GoToSettings()
@@ -23,5 +31,32 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    IEnumerator Fadeout()
+    {
+        // loop to fade out over 3 seconds
+        // Track how many seconds we've been fading.
+        float t = 0;
+
+        while (t < duration)
+        {
+            // Step the fade forward one frame.
+            t += Time.deltaTime;
+            // Turn the time into an interpolation factor between 0 and 1.
+            float blend = Mathf.Clamp01(t / duration);
+
+            // Blend to the corresponding opacity between start & target.
+            Fade.GetComponent<CanvasGroup>().alpha = blend;
+
+            // Wait one frame, and repeat.
+            yield return null;
+
+           
+        } 
+        yield return new WaitForSeconds(duration+1);
+        
+        SceneManager.LoadScene("Level 1");
+        //
     }
 }
